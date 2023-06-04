d60fc71a3e0d94776377088e276a98d0f5bdc2ae
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-06-04 23:05:11 +0200

    upgrade

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const userDistanceKm = document.getElementById("user-distance");
-console.log(userDistanceKm);
-const userAge = document.getElementById("user-age");
+const userNameInput = document.getElementById("user-name");
+const userDistanceInput = document.getElementById("user-distance");
+const userAgeInput = document.getElementById("user-age");
 const button = document.getElementById("submit");
-const basePrice = 0.21;
 
-const title = document.querySelector("h1");
+//ELEMENTI HTML OUTPUT
+const title = document.querySelector("h2");
+const ticketContainer = getElementById("ticket");
+const passengerName = document.getElementById("description-name");
+const offer = document.getElementById("description-offer");
+const cab = document.getElementById("description-cab");
+const cp = document.getElementById("description-cp");
+const price = document.getElementById("description-price");
 
+// SUL CLICK GENERA
 button.addEventListener("click", function () {
-  let priceTicket;
+  //PRELIEVO DEI DATI
+  const userName = userNameInput.value;
+  const userDistance = userDistanceInput.value;
+  const userAge = userAgeInput.value;
+
+  //IL CALCOLO
+  let kmPrice = 0.21;
   if (userAge.value < 18) {
-    priceTicket = userDistanceKm.value * basePrice * 0.8;
+    priceTicket = userDistance.value * kmPrice * 0.8;
   } else if (userAge >= 65) {
-    priceTicket = userDistanceKm.value * basePrice * 0.6;
+    priceTicket = userDistance.value * kmPrice * 0.6;
   } else {
-    priceTicket = userDistanceKm.value * basePrice;
+    priceTicket = userDistance.value * kmPrice;
   }
 
-  console.log(userDistanceKm.value, userAge.value);
-  title.innerHTML = `The PRICE of your ticket is ${priceTicket.toFixed(
+  //GENERA NR. CARROZZA
+  const cabNum = Math.floor(Math.random() * 10) + 1;
+  //GENERA CP
+  const cpCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+
+  //OUTPUT
+  ticketContainer.classList.remove("hidden");
+  passengerName.innerHTML = userName;
+  offer.innerHTML = userAge;
+  cab.innerHTML = cabNum;
+  cp.innerHTML = cpCode;
+  price.innerHTML = kmPrice.toFixed(2);
+  title.innerHTML = `THE PRICE OF YOUR TICKET IS ${priceTicket.toFixed(
     2
   )} EURO!`;
 });
